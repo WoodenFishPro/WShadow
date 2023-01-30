0dfde9830c66f376a53562a0aba0760d175b349d
89300734@qq.com <89300734@qq.com>
2023-01-30 12:07:23 +0800

    plugin-project工程已经跑通，相比原来的demo，需要增加aaptOptions选项

--- projects/sample/maven/plugin-project/plugin-app/src/main/java/com/tencent/shadow/sample/plugin/MainActivity.java
+++ projects/sample/maven/plugin-project/plugin-app/src/main/java/com/tencent/shadow/sample/plugin/MainActivity.java
@@ -2,11 +2,13 @@ package com.tencent.shadow.sample.plugin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "这是插件", Toast.LENGTH_LONG).show();
     }
 }
